23667984fb5b6427cd28a89b2c503f8e6c7bed97
Fadel Alamri <53441889+Lupus-Rexot@users.noreply.github.com>
2020-02-09 00:48:42 +0300

    (A) new corousel and icon
    icons from fontawesome

--- ia_test/test.py
+++ ia_test/test.py
@@ -1,1 +1,9 @@
-print('hello world')
+h1 style="font-family: cursive; color: black; padding:50px 0px 20px 500px;"> <span style="background-color: whitesmoke ; padding: 10px; border: black 1px solid;">About us</span></h1>
+
+                <div style="color: white;font-size: 16px; background-color: black; padding: 50px ;">
+                       <div >
+                        <h4> rate</h4>
+                       <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et eaque, magnam fugit dolorum, natus provident deserunt ea accusamus iusto corrupti 
+                        officiis, nostrum suscipit cum qui! At distinctio veniam commodi cumque.</P>
+                        </div>
+                </div>
