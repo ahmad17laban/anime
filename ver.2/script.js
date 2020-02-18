e464cfb17530d181975bba82542104dc9f103b18
Fadel Alamri <53441889+Lupus-Rexot@users.noreply.github.com>
2020-02-18 05:52:09 +0300

    Add files via upload
    updated

---
+++ ver.2/script.js
@@ -0,0 +1,36 @@
+function valid(){
+    var name = document.getElementById('inputName3').value
+    var email = document.getElementById('inputEmail3').value
+    var password = document.getElementById('inputPassword3').value
+    var c_password = document.getElementById('inputPassword4').value
+    var error_msg= document.getElementById('error_msg')
+    var text  
+    error_msg.style.padding= "10px"
+    if (name.length < 5){
+      text="plese enter valid name"
+      error_msg.innerHTML= text
+      return false
+    }
+    
+    if ( email.indexOf("@") == -1 ||name.length < 10){
+      text="plese enter valid Email"
+      error_msg.innerHTML= text
+      return false
+    }
+    if (password.length < 6 ){
+      text="plese enter valid passport"
+      error_msg.innerHTML= text
+      return false
+    }
+    if (password != c_password ){
+      text="plese enter valid passport"
+      error_msg.innerHTML= text
+      return false
+    }
+    alert('valid')
+    return true 
+
+
+
+
+}
