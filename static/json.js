09a5ab8e2db33f600c6e22311e732bdd8a1859b9
Fadel Alamri <53441889+Lupus-Rexot@users.noreply.github.com>
2020-03-10 11:42:37 +0300

    fixed path for all images

--- static/json.js
+++ static/json.js
@@ -1,15 +1,15 @@
 var cards = {
     card1: {
-        pic: "Img/A.png",
+        pic: "/Img/A.png",
         title: "TAttack on Titan: The True Origin of The Founding Titan, Ymir, Revealed",
         
     },
     card2: {
-        pic: "Img/B.jpg",
+        pic: "/Img/B.jpg",
         title: "TAttack on Titan: The True Origin of The Founding Titan, Ymir, Revealed",
         
     },card3: {
-        pic: "Img/c.jpg",
+        pic: "/Img/c.jpg",
         title: "TAttack on Titan: The True Origin of The Founding Titan, Ymir, Revealed",
         
     },
